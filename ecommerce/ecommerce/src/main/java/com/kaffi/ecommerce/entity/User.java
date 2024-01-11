3761814d9f0acfdb8f5c470fb555180f41eeb335
Oswaldo Fonseca <soy.oswaldofonseca@Gmail.com>
2024-01-11 14:29:12 -0600

    Proyecto backend springboot: paquetes correccion

--- ecommerce/ecommerce/src/main/java/com/kaffi/ecommerce/entity/User.java
+++ ecommerce/ecommerce/src/main/java/com/kaffi/ecommerce/entity/User.java
@@ -1,5 +1,15 @@
 package com.kaffi.ecommerce.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+@Entity
+@Table(name="usuarios")
 public class User {
 
 }
