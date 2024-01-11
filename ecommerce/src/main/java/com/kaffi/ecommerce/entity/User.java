1ca0c3a59d47422d51534db002f00461a0611b40
Oswaldo Fonseca <soy.oswaldofonseca@Gmail.com>
2024-01-11 16:10:14 -0600

    Proyecto backend etity: user & usertype

---
+++ ecommerce/src/main/java/com/kaffi/ecommerce/entity/User.java
@@ -0,0 +1,30 @@
+package com.kaffi.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+@Entity
+@Table(name="usuarios")
+public class User {
+	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(name="nombre",nullable = false,length = 25)
+	private String firtName;
+	@Column(name="apellido",nullable = false,length = 25)
+	private String lastName;
+	@Column(name="email",nullable = false,length = 30,unique = true)
+	private String email;
+	@Column(name="password",nullable = false,length = 45 )
+	private String password;
+	@Column(name="direccion",length = 45 )
+	private String address;
+	@Column(name="id_tipo_usuario",nullable = false)
+	private Long idUserType;
+
+}
---
+++ ecommerce/src/main/java/com/kaffi/ecommerce/entity/UserType.java
@@ -0,0 +1,22 @@
+package com.kaffi.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+@Entity
+@Table(name="tipos_usuarios")
+public class UserType {
+	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idUserType;
+	@Column(name="nombre_tipo",nullable = false,length = 25)
+	private String typeName;
+	@Column(name="descripcion",nullable = false,length = 45)
+	private String description;
+
+}
