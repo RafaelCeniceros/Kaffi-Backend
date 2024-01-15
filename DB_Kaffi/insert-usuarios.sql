76d964346e315b4a28c1385c9c9baa150d533618
Rafael Ceniceros <rrafa7114@gmail.com>
2024-01-15 12:42:36 -0600

    insert usuarios

--- DB_Kaffi/insert-usuarios.sql
+++ DB_Kaffi/insert-usuarios.sql
@@ -2,17 +2,17 @@
 
 SELECT * FROM usuarios;
 
-INSERT INTO usuarios (nombre, apellidos, email, `password`, direccion, id_tipo_usuario)
-	VALUES ('Oswaldo', 'Fonseca Mejia', 'leonNeumann.26121990@gmail.com', 'Code_Titans123', 'Beberly Hills 90210', 1);
-INSERT INTO usuarios (nombre, apellidos, email, `password`, direccion, id_tipo_usuario)
-	VALUES ('Fernando', 'Juarez Sanchez', 'juarezsfw@outlook.com', 'Code_Titans123', 'Fondo de bikin No.154', 1);
-INSERT INTO usuarios (nombre, apellidos, email, `password`, direccion, id_tipo_usuario)
+INSERT INTO usuarios (activo, nombre, apellidos, email, `password`, direccion, id_tipo_usuario)
+	VALUES (1, 'Oswaldo', 'Fonseca Mejia', 'leonNeumann.26121990@gmail.com', 'Code_Titans123', 'Beberly Hills 90210', 1);
+INSERT INTO usuarios (activo, nombre, apellidos, email, `password`, direccion, id_tipo_usuario)
+	VALUES (1, 'Fernando', 'Juarez Sanchez', 'juarezsfw@outlook.com', 'Code_Titans123', 'Fondo de bikin No.154', 1);
+INSERT INTO usuarios (activo, nombre, apellidos, email, `password`, direccion, id_tipo_usuario)
 	VALUES ('Alan', 'Villa Ocampo', 'zailancoc@gmail.com', 'Code_Titans123', 'Fondo de bikin No.897', 1);
-INSERT INTO usuarios (nombre, apellidos, email, `password`, direccion, id_tipo_usuario)
-	VALUES ('Rafael', 'Ceniceros Ceballos', 'rrafa7114@gmail.com', 'Code_Titans123', 'Fondo de bikin No.897', 1);
-INSERT INTO usuarios (nombre, apellidos, email, `password`, direccion, id_tipo_usuario)
-	VALUES ('Cristiano Ronaldo', 'Dos Santos Aveiro', 'CR7_oficial@gmail.com', 'Code_Titans123', 'Al Nassir 15432 Arabia SAudita', 2);
-INSERT INTO usuarios (nombre, apellidos, email, `password`, direccion, id_tipo_usuario)
-	VALUES ('Andrés Manuel', 'López Obrador', 'AMLO_4T@gmail.com', 'Code_Titans123', 'Palacio Nacional CDMX', 2);
-INSERT INTO usuarios (nombre, apellidos, email, `password`, direccion, id_tipo_usuario)
-	VALUES ('Isaac', 'Newton', 'fisicomuerto@gmail.com', 'Code_Titans123', 'Woolsthorpe Manor Hous', 2);
+INSERT INTO usuarios (activo, nombre, apellidos, email, `password`, direccion, id_tipo_usuario)
+	VALUES (1, 'Rafael', 'Ceniceros Ceballos', 'rrafa7114@gmail.com', 'Code_Titans123', 'Fondo de bikin No.897', 1);
+INSERT INTO usuarios (activo, nombre, apellidos, email, `password`, direccion, id_tipo_usuario)
+	VALUES (1, 'Cristiano Ronaldo', 'Dos Santos Aveiro', 'CR7_oficial@gmail.com', 'Code_Titans123', 'Al Nassir 15432 Arabia SAudita', 2);
+INSERT INTO usuarios (activo, nombre, apellidos, email, `password`, direccion, id_tipo_usuario)
+	VALUES (1, 'Andrés Manuel', 'López Obrador', 'AMLO_4T@gmail.com', 'Code_Titans123', 'Palacio Nacional CDMX', 2);
+INSERT INTO usuarios (activo, nombre, apellidos, email, `password`, direccion, id_tipo_usuario)
+	VALUES (1, 'Isaac', 'Newton', 'fisicomuerto@gmail.com', 'Code_Titans123', 'Woolsthorpe Manor Hous', 2);
