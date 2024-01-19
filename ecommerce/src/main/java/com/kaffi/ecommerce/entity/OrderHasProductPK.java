5d0011bdf554b7eba60fc86eb1229328ef2e0ead
Oswaldo Fonseca <soy.oswaldofonseca@Gmail.com>
2024-01-18 18:18:44 -0600

    proyecto para deploy

--- ecommerce/src/main/java/com/kaffi/ecommerce/entity/OrderHasProductPK.java
+++ ecommerce/src/main/java/com/kaffi/ecommerce/entity/OrderHasProductPK.java
@@ -12,6 +12,5 @@ public class OrderHasProductPK implements Serializable {
     private static final long serialVersionUID = 1L; // Puedes elegir cualquier valor que desees
 
     private Long order; // El nombre debe coincidir con el campo en la clase OrderhasProduct
-
     private Long product; // El nombre debe coincidir con el campo en la clase OrderhasProduct
 }
